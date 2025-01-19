5bdc15cdff86bf6bfc617f473bff65f59b05083d
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-01-19 15:15:51 +0000

    streamer class based views added

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from movies_app.views import movies_list, movie_details
+from movies_app.views import movies_list, movie_details, StreamerDetailAV, StreamAV
 
 urlpatterns = [
     path("api/", include("movies_app.api.urls") ),
     path("list/", movies_list, name="movies-list"),
     path("list/<int:pk>/", movie_details, name="movies-details"),
+    path("stream/list/", StreamAV.as_view(), name="stream-list"),
+    path("stream/list/<int:pk>/", StreamerDetailAV.as_view(), name="stream-details"),
 ]
--- movies_app/views.py
+++ movies_app/views.py
@@ -1,11 +1,62 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from movies_app.models import Movie,Streamer, Review
+from movies_app.models import Movie, Streamer, Review
 from rest_framework.decorators import api_view # function based views
 from rest_framework.views import APIView # class based views
-from movies_app.serializers import MovieSerializer
+from movies_app.serializers import MovieSerializer, StreamerSerializer, ReviewSerializer
 from rest_framework.response import Response
 from rest_framework import status
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle, ScopedRateThrottle
+
+
+class StreamAV(APIView):
+    # permission_classes = [IsAdminOrReadOnly]
+    throttle_classes = [AnonRateThrottle]
+
+    def get(self, request):
+        platform = Streamer.objects.all()
+        serializer = StreamerSerializer(platform, 
+                                        many=True, 
+                                        context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StreamerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class StreamerDetailAV(APIView):
+    # permission_classes = [IsAdminOrReadOnly]
+    throttle_classes = [AnonRateThrottle]
+
+    def get(self, request, pk):
+        try:
+            platform = Streamer.objects.get(pk=pk)
+        except Streamer.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = StreamerSerializer(
+            platform, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        platform = Streamer.objects.get(pk=pk)
+        serializer = StreamerSerializer(platform, 
+                                              data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        platform = Streamer.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET','POST'])
